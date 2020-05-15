a1a217e30632144496545bbd67f3adf843a473da
Pradeep <mishrapradeep@759@gmail.com>
2020-05-15 15:41:33 +0530

    first vue tutorial

---
+++ app.js
@@ -0,0 +1,8 @@
+var myview = new Vue({
+    el: "#demo-id",
+    data: {
+        message: "This is my first vue tutorial",
+        fname: "Pradeep",
+        channel: "Online Web Tutor"
+    }
+})
